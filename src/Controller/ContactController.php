69898c7fc63e14f34ea3ebcdc86470e1a65886d3
Lisa-Mhl <michellisa.75@gmail.com>
2020-05-04 16:05:48 +0200

    error and redirection ok

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,18 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aurelwcs
- * Date: 08/04/19
- * Time: 18:40
- */
 
 namespace App\Controller;
 
-use App\Model\ContactManager;
-
 class ContactController extends AbstractController
 {
-
     /**
      * Display home page
      *
@@ -21,8 +12,40 @@ class ContactController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
+
     public function index()
     {
-        return $this->twig->render('Contact/index.html.twig');
+        $error = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (!empty($_POST['email']) && !empty($_POST['name']) && !empty($_POST['subject']) && !empty($_POST['message'])) {
+                $to = "michellisa.75@gmail.com";
+                $from = "lilouuuups@gmail.com";
+
+                $email = $_POST['email'];
+                $message = $_POST['message'];
+                $subject = $_POST['subject'];
+                $name = $_POST['name'];
+
+                $content = "Objet:" . $subject . "<br>" . "Nom:" . $name . "<br>" . "Email:" . $email . "<br>" . "Message:" . $message;
+                $headers = "from: " . $from;
+                mail($to, $subject, $content, $headers);
+                return $this->twig->render(
+                    'Contact/message.html.twig',
+                    [
+                        'name' => $name,
+                        'subject' => $subject,
+                    ]
+                );
+            }
+        }
+        if (empty($_POST['email']) || empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message'])) {
+            $error = "Tous les champs sont obligatoires";
+        }
+        return $this->twig->render(
+            'Contact/index.html.twig',
+            [
+                'error' => $error
+            ]
+        );
     }
 }
